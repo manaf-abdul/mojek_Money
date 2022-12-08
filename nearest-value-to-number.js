fdf27403e82b1fabaee142a714a243a1c1360ec3
manaf-abdul <abdulmanafp1996@gmail.com>
2022-12-08 20:03:55 +0530

    CHANGES

--- nearest-value-to-number.js
+++ nearest-value-to-number.js
@@ -1,20 +1,20 @@
-let absValue=Math.abs
-
-
-let M=16,N=5
+let num1=13,num2=4
 let multi=1;
 let prev=0;
 let next=0;
 
+let absValue=Math.abs
+let prevDifference=absValue(num1-prev)
+
 while(true){
-    next=multi*N;
+    next=multi*num2;
     multi++
 
-    const nextDifference=absValue(M-next)
-    const prevDifference=absValue(M-prev)
+    const nextDifference=absValue(num1-next)
 
     if(nextDifference<prevDifference){
         prev=next;
+        prevDifference=nextDifference;
         continue;
     }
     break;
